1b19e0fb334bb9f2c65337ba5f294bc291bfb980
fizz <393193646@qq.com>
2017-12-22 23:03:46 +0800

    exception def

---
+++ src/main/java/com/fizz/exception/SellException.java
@@ -0,0 +1,20 @@
+package com.fizz.exception;
+
+import com.fizz.Enums.ResultEnum;
+
+/**
+ * @author fizz
+ * @since 2017/12/19 21:10
+ */
+public class SellException extends RuntimeException {
+
+	private static final long serialVersionUID = -5870462106719690868L;
+
+	private Integer code;
+
+	public SellException(ResultEnum resultEnum) {
+		super(resultEnum.getMsg());
+		code = resultEnum.getCode();
+	}
+
+}
